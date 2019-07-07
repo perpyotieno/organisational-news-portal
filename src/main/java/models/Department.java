ffe4ea7df28fb800399174b593609ddd448861e3
Perpetua Mary <perpetuamary@yahoo.com>
2019-07-07 15:17:57 +0300

    Initial commit

---
+++ src/main/java/models/Department.java
@@ -0,0 +1,68 @@
+//package models;
+//
+//import java.util.Objects;
+//
+//public class Department {
+//
+//    private String name;
+//    private String description;
+//    private int numberOfEmployees;
+//    private  int id;
+//
+//    public Department(String name, String description, int numberOfEmployees){
+//        this.name = name;
+//        this.description= description;
+//        this.numberOfEmployees= numberOfEmployees;
+//
+//
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public String getDescription() {
+//        return description;
+//    }
+//
+//    public void setDescription(String description) {
+//        this.description = description;
+//    }
+//
+//    public int getNumberOfEmployees() {
+//        return numberOfEmployees;
+//    }
+//
+//    public void setNumberOfEmployees(int numberOfEmployees) {
+//        this.numberOfEmployees = numberOfEmployees;
+//    }
+//
+//    public int getId() {
+//        return id;
+//    }
+//
+//    public void setId(int id) {
+//        this.id = id;
+//    }
+//
+//    @Override
+//    public boolean equals(Object o) {
+//        if (this == o) return true;
+//        if (!(o instanceof Department)) return false;
+//        Department that = (Department) o;
+//        return getNumberOfEmployees() == that.getNumberOfEmployees() &&
+//                getId() == that.getId() &&
+//                getName().equals(that.getName()) &&
+//                getDescription().equals(that.getDescription());
+//    }
+//
+//    @Override
+//    public int hashCode() {
+//        return Objects.hash(getName(), getDescription(), getNumberOfEmployees(), getId());
+//    }
+//
+//}
---
+++ src/test/java/models/DepartmentTest.java
@@ -0,0 +1,79 @@
+//package models;
+//
+//import org.junit.After;
+//import org.junit.Before;
+//import org.junit.Test;
+//
+//import static org.junit.Assert.*;
+//
+//public class DepartmentTest {
+//
+//    @Before
+//    public void setUp() throws Exception {
+//    }
+//
+//    @After
+//    public void tearDown() throws Exception {
+//    }
+//
+//    @Test
+//    public void getNameReturnsCorrectName() throws Exception {
+//        Department testDepartment= setupDepartment();
+//        assertEquals("finance", testDepartment.getName());
+//    }
+//
+//    @Test
+//    public void setNameSetsCorrectName() throws Exception {
+//        Department testDepartment= setupDepartment();
+//        testDepartment.setName("finance");
+//        assertNotEquals("accounting", testDepartment.getName());
+//    }
+//
+//    @Test
+//    public void getDescriptionReturnsCorrectDescription() throws Exception{
+//        Department testDepartment = setupDepartment();
+//        assertEquals("handle finance business", testDepartment.getDescription());
+//    }
+//
+//    @Test
+//    public void setDescriptionSetsCorrectDescription() throws Exception {
+//        Department testDepartment= setupDepartment();
+//        testDepartment.setDescription("handle finance business");
+//        assertNotEquals("handle accounting business", testDepartment.getDescription());
+//    }
+//
+//    @Test
+//    public void getNumberOfEmployeesReturnsCorrectNumberofEmployee() {
+//        Department testDepartment = setupDepartment();
+//        assertEquals(12, testDepartment.getNumberOfEmployees());
+//    }
+//
+//    @Test
+//    public void setNumberOfEmployeesSetsCorrectNumber() {
+//        Department testDepartment= setupDepartment();
+//        testDepartment.setNumberOfEmployees(12);
+//        assertNotEquals(13, testDepartment.getNumberOfEmployees());
+//    }
+//
+//    @Test
+//    public void getId() {
+//    }
+//
+//    @Test
+//    public void setId() {
+//
+//    }
+//
+//    @Test
+//    public void equals1() {
+//    }
+//
+//    @Test
+//    public void hashCode1() {
+//    }
+//
+//    //helper
+//    public  Department setupDepartment(){
+//        return new Department("finance","handle finance business",12);
+//    }
+//}
