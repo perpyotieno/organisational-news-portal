2adb580d72ff0784cc0c0d71fd6f7510104d5144
Perpetua Mary <perpetuamary@yahoo.com>
2019-07-08 09:00:21 +0300

    create a department class and it's test class. Generate the tests for getter and setters for the department class. Generate the getters and setters.

--- src/main/java/models/Department.java
+++ src/main/java/models/Department.java
@@ -1,68 +1,78 @@
-//package models;
-//
-//import java.util.Objects;
-//
-//public class Department {
-//
-//    private String name;
-//    private String description;
-//    private int numberOfEmployees;
-//    private  int id;
-//
-//    public Department(String name, String description, int numberOfEmployees){
-//        this.name = name;
-//        this.description= description;
-//        this.numberOfEmployees= numberOfEmployees;
-//
-//
-//    }
-//
-//    public String getName() {
-//        return name;
-//    }
-//
-//    public void setName(String name) {
-//        this.name = name;
-//    }
-//
-//    public String getDescription() {
-//        return description;
-//    }
-//
-//    public void setDescription(String description) {
-//        this.description = description;
-//    }
-//
-//    public int getNumberOfEmployees() {
-//        return numberOfEmployees;
-//    }
-//
-//    public void setNumberOfEmployees(int numberOfEmployees) {
-//        this.numberOfEmployees = numberOfEmployees;
-//    }
-//
-//    public int getId() {
-//        return id;
-//    }
-//
-//    public void setId(int id) {
-//        this.id = id;
-//    }
-//
-//    @Override
-//    public boolean equals(Object o) {
-//        if (this == o) return true;
-//        if (!(o instanceof Department)) return false;
-//        Department that = (Department) o;
-//        return getNumberOfEmployees() == that.getNumberOfEmployees() &&
-//                getId() == that.getId() &&
-//                getName().equals(that.getName()) &&
-//                getDescription().equals(that.getDescription());
-//    }
-//
-//    @Override
-//    public int hashCode() {
-//        return Objects.hash(getName(), getDescription(), getNumberOfEmployees(), getId());
-//    }
-//
-//}
+package models;
+
+import java.util.Objects;
+
+public class Department {
+
+    private String name;
+    private String description;
+    private int numberOfEmployees;
+    private  int id;
+    private int userId;
+
+    public Department(String name, String description, int numberOfEmployees, int userId){
+        this.name = name;
+        this.description= description;
+        this.numberOfEmployees= numberOfEmployees;
+        this. userId = userId;
+
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getNumberOfEmployees() {
+        return numberOfEmployees;
+    }
+
+    public void setNumberOfEmployees(int numberOfEmployees) {
+        this.numberOfEmployees = numberOfEmployees;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Department)) return false;
+        Department that = (Department) o;
+        return getNumberOfEmployees() == that.getNumberOfEmployees() &&
+                getId() == that.getId() &&
+                getUserId() == that.getUserId() &&
+                getName().equals(that.getName()) &&
+                getDescription().equals(that.getDescription());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getName(), getDescription(), getNumberOfEmployees(), getId(), getUserId());
+    }
+}
--- src/main/java/models/Users.java
+++ src/main/java/models/Users.java
@@ -1,4 +1,47 @@
-package models;
-
-public class Users {
-}
+//package models;
+//
+//public class Users {
+//    private String name;
+//    private  String role;
+//    private String position;
+//    private int id;
+//
+//
+//    public Users(String name, String role, String position){
+//        this.name = name;
+//        this.role = role;
+//        this.position =position;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public String getRole() {
+//        return role;
+//    }
+//
+//    public void setRole(String role) {
+//        this.role = role;
+//    }
+//
+//    public String getPosition() {
+//        return position;
+//    }
+//
+//    public void setPosition(String position) {
+//        this.position = position;
+//    }
+//
+//    public int getId() {
+//        return id;
+//    }
+//
+//    public void setId(int id) {
+//        this.id = id;
+//    }
+//}
